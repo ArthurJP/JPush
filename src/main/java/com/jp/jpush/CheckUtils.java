1ce2470931e6d9977ae7cae4edeedb85e7ce24e2
张俊鹏 <zhangtylord@gmail.com>
2018-03-15 16:53:57 +0800

    recode

--- src/main/java/com/jp/jpush/CheckUtils.java
+++ src/main/java/com/jp/jpush/CheckUtils.java
@@ -2,14 +2,13 @@ package com.jp.jpush;
 
 import org.jetbrains.annotations.*;
 
-import org.apache.http.util.TextUtils;
-
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class CheckUtils {
-    public static boolean isEmpty(String s) {
+    @Contract(value = "null -> true", pure = true)
+    private static boolean isEmpty(String s) {
         if (null == s)
             return true;
         if (s.length() == 0)
@@ -17,42 +16,49 @@ public class CheckUtils {
         return s.trim().length() == 0;
     }
 
+    @Contract(value = "null -> true", pure = true)
+    private static boolean isEmpty(List<String> s) {
+        boolean result = true;
+        for (String x : s) {
+            if (!isEmpty( x ))
+                result = false;
+        }
+        return result;
+    }
+
     /**
      * 只能以 “+” 或者 数字开头；后面的内容只能包含 “-” 和 数字。
      */
     private final static String MOBILE_NUMBER_CHARS = "^[+0-9][-0-9]{1,}$";
+    private final static String TAG_ALIA_CHARS = "^[\u4E00-\u9FA50-9a-zA-Z_!@#$&*+=.|]+$";
+
+    @Contract(value = "null ,null ->false", pure = true)
+    private static boolean match(String s, String regex) {
+        Pattern p = Pattern.compile( regex );
+        Matcher m = p.matcher( s );
+        return m.matches();
+    }
 
     @Contract(value = "null -> false", pure = true)
     public static boolean isValidMobileNumber(String s) {
-        if (TextUtils.isEmpty( s )) return false;
-        Pattern p = Pattern.compile( MOBILE_NUMBER_CHARS );
-        Matcher m = p.matcher( s );
-        return m.matches();
+        if (isEmpty( s )) return false;
+        return match( s, MOBILE_NUMBER_CHARS );
     }
 
     // 校验Tag Alias 只能是数字,英文字母和中文
     @Contract(value = "null ->false", pure = true)
     static boolean isValidTagAndAlias(String s) {
-        if (s.isEmpty())
+        if (isEmpty( s ))
             return false;
-        Pattern p = Pattern.compile( "^[\u4E00-\u9FA50-9a-zA-Z_!@#$&*+=.|]+$" );
-        Matcher m = p.matcher( s );
-        if (!m.matches()) {
-            return false;
-        }
-        return true;
+        return match( s, TAG_ALIA_CHARS );
     }
 
     @Contract(value = "null ->false", pure = true)
     static boolean isValidTagAndAlias(List<String> s) {
-        if (s.isEmpty())
+        if (isEmpty( s.toString() ))
             return false;
         for (String x : s) {
-            Pattern p = Pattern.compile( "^[\u4E00-\u9FA50-9a-zA-Z_!@#$&*+=.|]+$" );
-            Matcher m = p.matcher( x );
-            if (!m.matches()) {
-                return false;
-            }
+            if (match( x, TAG_ALIA_CHARS )) return false;
         }
         return true;
     }
--- src/main/java/com/jp/jpush/Test.java
+++ src/main/java/com/jp/jpush/Test.java
@@ -4,8 +4,10 @@ import java.util.Collections;
 
 public class Test {
     public static void main(String[] args) {
-        PushUtils.push( Collections.singletonList( "110" ),"IOS信息发送测试",null);
-        PushUtils.pushByTags( Collections.singletonList( "120" ),"IOS标签组测试",null );
-        PushUtils.pushToAll( "IOS全体推送测试" ,null);
+        PushUtils.push( "110" ,"IOS信息发送测试",null);
+//        PushUtils.push( Collections.singletonList( "110" ),"IOS信息发送测试",null);
+        PushUtils.pushByTags( "120" ,"IOS标签组测试",null );
+//        PushUtils.pushByTags( Collections.singletonList( "120" ),"IOS标签组测试",null );
+//        PushUtils.pushToAll( "IOS全体推送测试" ,null);
     }
 }
